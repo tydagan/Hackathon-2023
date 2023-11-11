525e24cbcc3f205b5960d02f3b716cc3574904bd
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2023-11-11 15:22:17 -0500

    router????!!!!

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // -dom?
 import Home from './Home';
 import Search from './Search';
 
