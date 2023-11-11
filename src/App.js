b56a903209dd9ec4773b7b4d592574293c2cee9a
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2023-11-11 01:31:28 -0500

    text

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <form action="/search" autocomplete="off" method="GET" role="search">
         <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
       </form>
+      <p id="info-text">Your lyrics are right here, waiting for you!</p>
     </div >
   );
 }
