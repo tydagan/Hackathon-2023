afb6f8b1f062cf572938e535e78d39aaac93d406
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2023-11-11 17:44:42 -0500

    images (now random)

--- src/Search.js
+++ src/Search.js
@@ -4,8 +4,22 @@ import { useNavigate } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
-
-
+window.onload = function() {
+    let id = Math.floor(Math.random() * 3);
+    switch (id) {
+        case 0:
+            document.getElementById('image').src = "../algo_rhythm1.png"
+            break;
+        case 1:
+            document.getElementById('image').src = "../algo_rhythm2.png"
+            break;
+        case 2:
+            document.getElementById('image').src = "../algo_rhythm3.png"
+            break;
+        default:
+            break;
+    }
+}
 
 const Result = ({ location }) => {
     const navigate = useNavigate();
@@ -18,12 +32,13 @@ const Result = ({ location }) => {
         <div className="Result">
             <header className="Result-header">
                 <a href="/"><h3 style={{ color: 'black' }}> ALGORHYTHMZ</h3></a>
+                {/* <a href="/"></a> */}
                 {/* <input type="text"></input> */}
             </header >
-            <img className="Album-image" src="../algo_rhythm1.png" alt="Algo Rhythm"></img>
+            <img className="Album-image" id="image" alt="Algo Rhythm"></img>
             <div className="Song-title">{word}</div>
             <div className="Song-subtitle">produced by ALGORHYTHMZ</div>
-            {/* <img src="" id="event-1"></img> */}
+            
             <div className="Song-lyrics">
                 [Verse 1]<br />
                 Number one, victory royale<br />
