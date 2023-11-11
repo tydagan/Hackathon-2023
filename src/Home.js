2c00d4d2e386916ae2f839e9135adc4a2aae37b4
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 12:25:08 -0500

    new stff

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 
 const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Do something with the input value, e.g., send it to an API, etc.
+        console.log('Input Value:', inputValue);
+    };
     return (
         <div className="Home">
             <header className="App-header">
                 <h3 style={{ color: 'black' }}> algorhythmz</h3>
             </header >
             <div><img src={logo} className="App-logo" alt="logo" /></div>
-            <form action="/search" autocomplete="off" method="GET" role="search">
-                <input name="q" type="text" inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
-            </form>
-            <button style={{ width: '100px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }}>
+            <form onSubmit={handleSubmit} action="/search" autocomplete="off" method="GET" role="search">
+                <input onChange={handleChange} name="q" type="text" inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
+            </form >
+            <button value={inputValue} style={{ width: '100px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }}>
                 <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
                     Start
                 </Link>
