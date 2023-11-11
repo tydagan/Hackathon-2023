0e3c765fa304a40d432928d0023386f4017c0446
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2023-11-11 13:18:20 -0500

    song formatting

--- src/Search.js
+++ src/Search.js
@@ -11,7 +11,17 @@ const Result = () => {
                 <h3 style={{ color: 'black' }}> ALGORHYTHMZ</h3>
             </header >
             <div className="album-box">insert album cover!</div>
-            <></>
+            <div className="Song-title">[SONG NAME]</div>
+            <div className="Song-subtitle">produced by ALGORHYTHMZ</div>
+
+            <div className="Song-lyrics">
+                [Verse 1]<br/>
+                Number one, victory royale<br/>
+                Yeah Fortnite, we bout to get down<br/>
+                (Get down)<br/>
+                Ten kills on the board right now, just wiped out Tomato Town
+            </div>
+
         </div>
     );
 };
