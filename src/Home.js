0bff11ffa32849925785a7f27d630f509ea80375
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 11:49:53 -0500

    New Button

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,6 @@ import './App.css';
 const Home = () => {
     return (
         <div className="Home">
-            <h2>Home Page</h2>
-            <p>Welcome to the home page!</p>
-            <button>
-                <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    Go to Search
-                </Link>
-            </button>
             <header className="App-header">
                 <h3 style={{ color: 'black' }}> algorhythmz</h3>
             </header >
@@ -20,7 +13,7 @@ const Home = () => {
             <form action="/search" autocomplete="off" method="GET" role="search">
                 <input name="q" type="text" inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
             </form>
-            <button style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }}>
+            <button style={{ width: '100px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif", }}>
                 <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
                     Start
                 </Link>
