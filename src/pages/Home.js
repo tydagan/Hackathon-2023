6f61cac12a0efcc4bd65361428db4375f858cd72
kaitlynmalsky <93287571+kaitlynmalsky@users.noreply.github.com>
2023-11-11 11:35:11 -0500

    css attempt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,19 @@
 import React from "react";
+import logo from '../logo.svg';
+import './App.css'
 
 function Home() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        h3 style = {
-            { color: 'black' } } > algorhythmz < /h3> <
-        /header > <
-        div > < img src = { "src/logo.svg" }
-        className = "App-logo"
-        alt = "logo" / > < /div> <
-        form action = "/search"
-        autocomplete = "off"
-        method = "GET"
-        role = "search" >
-        <
-        input name = "q"
-        type = "text"
-        placeholder = "Search..."
-        inputmode = "search"
-        style = {
-            { width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" } }
-        /> <
-        /form> <
-        p id = "info-text" > Your lyrics are right here, waiting
-        for you! < /p> <
-        /div >
+    return (
+        <Home>
+        <header className="App-header">
+          <h3 style={{ color: 'black' }}> algorhythmz</h3>
+        </header >
+        <div><img src={logo} className="App-logo" alt="logo" /></div>
+        <form action="/search" autocomplete="off" method="GET" role="search">
+          <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
+        </form>
+        <p id="info-text">Your lyrics are right here, waiting for you!</p>
+      </Home >
     );
 }
 
