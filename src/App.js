2d6018354140f5929ffa7b1a42155a8fc91f4698
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 01:11:03 -0500

    header

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div><h1 style={{ color: '#ea4d3d' }}> algorhythmz</h1></div>
-        <div><img src={logo} className="App-logo" alt="logo" /></div>
-        <form action="/search" autocomplete="off" method="GET" role="search">
-          <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", color: "'#ea4d3d'", fontFamily: "sans-serif" }} />
-        </form>
       </header >
+      <div><img src={logo} className="App-logo" alt="logo" /></div>
+      <form action="/search" autocomplete="off" method="GET" role="search">
+        <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
+      </form>
     </div >
   );
 }
