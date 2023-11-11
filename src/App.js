cc8e98faae69adb2b254a412a917503ae0e9829f
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 00:58:59 -0500

    bigger search bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{ color: '#ff0000' }}> algorhythmz</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div><h1 style={{ color: '#ea4d3d' }}> algorhythmz</h1></div>
+        <div><img src={logo} className="App-logo" alt="logo" /></div>
         <form action="/search" autocomplete="off" method="GET" role="search">
-          <input name="q" type="text" placeholder="Search..." inputmode="search" />
+          <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", color: "'#ea4d3d'", fontFamily: "sans-serif" }} />
         </form>
-      </header>
+      </header >
     </div >
   );
 }
