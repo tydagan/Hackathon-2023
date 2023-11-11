d6b2378b980794003f2826b956fd9da88f38a506
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 11:36:06 -0500

    Arranging

--- src/Home.js
+++ src/Home.js
@@ -6,20 +6,18 @@ import './App.css';
 const Home = () => {
     return (
         <div>
-            <h2>Home Page</h2>
-            <p>Welcome to the home page!</p>
-            <button>
-                <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    Go to Search
-                </Link>
-            </button>
             <header className="App-header">
                 <h3 style={{ color: 'black' }}> algorhythmz</h3>
             </header >
             <div><img src={logo} className="App-logo" alt="logo" /></div>
             <form action="/search" autocomplete="off" method="GET" role="search">
-                <input name="q" type="text" placeholder="Search..." inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
+                <input name="q" type="text" inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
             </form>
+            <button>
+                <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    Search
+                </Link>
+            </button>
             <p id="info-text">Your lyrics are right here, waiting for you!</p>
         </div >
     );
