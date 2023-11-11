c01b750213373a634892adcb11f0db98b78103b1
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2023-11-11 12:30:19 -0500

    new home

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
@@ -17,22 +15,51 @@ const Home = () => {
         // Do something with the input value, e.g., send it to an API, etc.
         console.log('Input Value:', inputValue);
     };
+
     return (
         <div className="Home">
             <header className="App-header">
                 <h3 style={{ color: 'black' }}> algorhythmz</h3>
-            </header >
-            <div><img src={logo} className="App-logo" alt="logo" /></div>
-            <form onSubmit={handleSubmit} action="/search" autocomplete="off" method="GET" role="search">
-                <input onChange={handleChange} name="q" type="text" inputmode="search" style={{ width: '500px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }} />
-            </form >
-            <button value={inputValue} style={{ width: '100px', height: "30px", "border-radius": "10px", fontFamily: "sans-serif" }}>
-                <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
-                    Start
-                </Link>
-            </button>
+            </header>
+            <div>
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <form
+                onSubmit={handleSubmit}
+                action="/search"
+                autoComplete="off"
+                method="GET"
+                role="search"
+            >
+                <input
+                    value={inputValue}
+                    onChange={handleChange}
+                    name="q"
+                    type="text"
+                    inputMode="search"
+                    style={{
+                        width: '500px',
+                        height: '30px',
+                        borderRadius: '10px',
+                        fontFamily: 'sans-serif',
+                    }}
+                />
+                <button
+                    type="submit"
+                    style={{
+                        width: '100px',
+                        height: '30px',
+                        borderRadius: '10px',
+                        fontFamily: 'sans-serif',
+                    }}
+                >
+                    <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        Start
+                    </Link>
+                </button>
+            </form>
             <p id="info-text">Your lyrics are right here, waiting for you!</p>
-        </div >
+        </div>
     );
 };
 
